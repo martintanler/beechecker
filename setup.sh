f062457c017f5bf39964962108e162b1cee5bd50
martintanler <martin.tanler@gmail.com>
2019-11-26 23:55:59 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 sudo apt-get update
-sudo apt-get dist-upgrade
+sudo apt-get -y dist-upgrade
 sudo apt-get install sqlite3
 sudo apt-get install python3-pip
 sudo python3 -m pip install --upgrade pip setuptools wheel
