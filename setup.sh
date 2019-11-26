b6a568a19b8d68ce66165a7c3c83810f105f0d39
martintanler <martin.tanler@gmail.com>
2019-11-26 23:34:19 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ sudo apt-get dist-upgrade
 sudo apt-get install sqlite3
 sudo apt-get install python3-pip
 sudo python3 -m pip install --upgrade pip setuptools wheel
+sudo pip3 install requests
 cd /home/pi
 git clone https://github.com/adafruit/Adafruit_Python_DHT.git
 cd Adafruit_Python_DHT
